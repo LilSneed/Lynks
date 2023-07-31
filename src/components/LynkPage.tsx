1e0daf2315202c96305487e4da827079df188d5e
Lil Sneed <rerumnovarum77@gmail.com>
2023-07-31 02:35:44 +0200

    db update, related clusters ui

--- src/components/LynkPage.tsx
+++ src/components/LynkPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Lynk from "./Lynk";
+import RelatedMenu from "./RelatedMenu";
 
 export default function LynkPage({
   lynks,
@@ -11,20 +12,48 @@ export default function LynkPage({
   clusterData: Array<any>;
   key: number;
 }) {
+  const PhRelated = [
+    {
+      id: 1,
+      creatorId: 1,
+      image: "clusterImage",
+      url: "clusterUrl",
+      title: "PH Cluster",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et arcu vel leo dictum laoreet sed vulputate nulla. Nullam vitae congue nisi. Donec ultrices orci in pharetra dictum placerat.",
+    },
+    {
+      id: 2,
+      creatorId: 1,
+      image: "clusterImage",
+      url: "clusterUrl",
+      title: "PH Cluster2",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et arcu vel leo dictum laoreet sed vulputate nulla. Nullam vitae congue nisi. Donec ultrices orci in pharetra dictum placerat.",
+    },
+  ];
+
   return (
     <div className="container flex flex-col mt-10 mb-5" key={key}>
-      <div className="flex justify-center">
+      <div className="flex justify-center flex-col">
+        <div className="self-start ">
+          <RelatedMenu relatedClusters={PhRelated} />
+        </div>
+
         <Image
           src={"https://placehold.co/600x600/pngplaceholder.com/600x600.png"}
           alt="User Image"
           width={200}
           height={200}
-          className="rounded-full"
+          className="rounded-full self-center"
         />
       </div>
       <h2 className="self-center mt-2 scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
         {clusterData[0].title}
       </h2>
+      <p className="self-center leading-7 [&:not(:first-child)]:mt-6">
+        {clusterData[0].description}
+      </p>
       {lynks.map((lynk) => (
         <Lynk
           id={lynk.id}
---
+++ src/components/RelatedMenu.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export default function RelatedMenu({
+  relatedClusters,
+}: {
+  relatedClusters: Array<any>;
+}) {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Related Clusters</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {relatedClusters.map((cluster) => (
+                <ListItem
+                  key={cluster.id}
+                  title={cluster.title}
+                  href={cluster.url}
+                >
+                  {cluster.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
